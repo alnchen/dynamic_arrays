47c2d0a2c996da2fc699a74709e3c58d984fa060
Allen Chen <alchen1231@gmail.com>
2017-06-13 14:14:03 -0700

    instructions added

---
+++ well_formed_string.rb
@@ -0,0 +1,15 @@
+# Given a sorted array consisting of only integers where every element appears
+# twice except for one element which appears once. Find this single element
+# that apprears only once. Do it on O(logn) time and O(1)
+
+# [2:12]
+# A string with the characters [,],{,},(,) is said to be well-formed
+# if the different types of brackets match in the correct order.
+
+# For example, ([ ]) {( )} is well-formed, but [(]{)} is not.
+
+# Write a function to test whether a string is well-formed.
+
+def well_formed?(str)
+
+end
