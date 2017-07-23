112d2f3c3df7ea7e6de3e0f1832f9cbfe34be5ab
Allen Chen <alchen1231@gmail.com>
2017-07-22 17:55:58 -0700

    updates

--- well_formed_string.rb
+++ well_formed_string.rb
@@ -6,6 +6,17 @@
 
 # Write a function to test whether a string is well-formed.
 
-def well_formed?(str)
+def well_formed(str)
+  left_chars = []
+  lookup = { '(' => ')', '['=> ']', '{'=> '}' }
 
+
+  str.chars.each do |char|
+    if lookup.keys.include?(char)
+      left_chars << char
+    elsif left_chars.length == 0 || lookup[left_chars.pop] != char
+      return false
+    end
+  end
+  return left_chars.empty?
 end
